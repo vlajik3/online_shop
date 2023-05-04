26c4b37109bcfbfb78111de93ada12f532d21b61
Tselishchev Vladislav <celishchevva@yandex.ru>
2023-05-04 20:14:35 +0300

    SERVER - [added] models of db tables, test route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'WORKING WELL' });
+});
 
 const PORT = process.env.PORT || 5000;
 

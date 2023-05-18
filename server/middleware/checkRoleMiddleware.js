2b9a1339b8f9b0b257e0b56d94f3c29d2e32422a
Tselishchev Vladislav <celishchevva@yandex.ru>
2023-05-18 18:33:52 +0300

    SERVER - [fixed] check role middleware

--- server/middleware/checkRoleMiddleware.js
+++ server/middleware/checkRoleMiddleware.js
@@ -12,7 +12,7 @@ module.exports = function (role) {
             }
             const decoded = jwt.verify(token, process.env.SECRET_KEY);
             if (decoded.role !== role) {
-                res.status(403).json({ message: 'Нет прав доступа' });
+                return res.status(403).json({ message: 'Нет прав доступа' });
             }
             req.user = decoded;
             next();
